d699638f43c7a3ece1c53f42ebf50fee1a9e8c67
Elias Tandel Barrionovo <elias.tandel@gmail.com>
2016-03-10 17:43:29 -0300

    Create version 0.2.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='JSONBender',
-    version='0.1',
+    version='0.2',
     description='Library for transforming dicts.',
     packages=['jsonbender'],
 )
